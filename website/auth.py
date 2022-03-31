5da8426ea9c138dbd169355591cb935af532cd0f
Jennifer Chhie <jschhie@ucdavis.edu>
2022-03-30 17:42:11 -0700

    init commit: bootstrap nav bar, login/reg forms

---
+++ website/auth.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, render_template, request
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    data = request.form
+    print(data)
+    return render_template("login.html")
+
+
+
+@auth.route('/logout')
+def logout():
+    return render_template("logout.html") # temp
+
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template("register.html")

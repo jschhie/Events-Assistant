9a2eba00027fd28824fe604a178f0efd7baaaad5
Jennifer Chhie <jschhie@ucdavis.edu>
2022-03-30 19:38:17 -0700

    connect to sqlite, defined db models

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,37 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "tasks_database.db"
+
+
 
 def create_app():
+    """ create flask web app, connect to DB, and 
+    register blueprints into app """
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'some secret key here'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    db.init_app(app)
 
     # register blueprints into app
     from .views import views
     from .auth import auth
 
+    # create or retrieve existing database
+    from .models import Task
+    create_database(app)
+
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     
     return app
+
+
+
+def create_database(app):
+    """ check if the database instance has already been made """
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
---
+++ website/models.py
@@ -0,0 +1,19 @@
+from . import db
+from flask_login import UserMixin
+
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    due_date = db.Column(db.DateTime, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True) 
+    username = db.Column(db.String(150), unique=True) 
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    tasks = db.relationship('Task')
